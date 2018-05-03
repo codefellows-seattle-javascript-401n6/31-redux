bc35311c0600c5bf0d20788e251ad4b77d24337b
amgranad <ambergkim@gmail.com>
2018-05-03 14:18:20 -0700

    readme

--- lab-amber/src/reducers/category-app.js
+++ lab-amber/src/reducers/category-app.js
@@ -18,8 +18,6 @@ export default function categoryReducer(state, action) {
   let currentCategories;
   let categoryIndex;
 
-  console.log('action type', action.type);
-
   switch(action.type) {
     case CATEGORY_CREATE:
       currentCategories = state.categories.slice();
@@ -27,28 +25,21 @@ export default function categoryReducer(state, action) {
       currentCategories.push(newCategory);
       return Object.assign(newState, state, {categories: currentCategories});
     case CATEGORY_UPDATE:
-      console.log('attempt to update values', action.values);
-      console.log('attempt to update values id', action.values.id);
       currentCategories = state.categories.slice();
       let categoryToUpdate = currentCategories.find(category => {
         return category.id === action.values.id;
        });
       categoryIndex = currentCategories.indexOf(categoryToUpdate);
-      console.log('category to update index', categoryIndex);
       currentCategories[categoryIndex].isEditing = !currentCategories[categoryIndex].isEditing;
       if (action.values.name) {
-        console.log('THERE is action values name');
         currentCategories[categoryIndex].name = action.values.name;
       }
       if (action.values.budget) {
-        console.log('THERE is action values budget');
         currentCategories[categoryIndex].budget = action.values.budget;
       }
-      console.log('edited category', currentCategories[categoryIndex]);
       return Object.assign(newState, state, {categories: currentCategories});
     case CATEGORY_DESTROY:
       currentCategories = state.categories.slice();
-      console.log('destroy', action.id);
       let categoryToRemove = currentCategories.find(category => {
         return category.id === action.id;
       });
