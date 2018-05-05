1429b91d241f140dcacb4e68514b8a8f2262de18
rjtj2007 <teacherman1@gmail.com>
2018-05-05 14:56:34 -0700

    built out dashboard and cat-actions files

---
+++ lab-ryan/src/components/dashboard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {
+    categoryCreate,
+    categoryUpdate,
+    categoryDestroy,
+} from '..action/category-actions.js';
+
+import CategoryList from './category-list.jsx';
+import CategoryForm from './category-form.jsx';
+
+class Dashboard extends React. Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+           <h1>{this.props.addName}</h1>
+           <CategoryForm createCategory={thisp.props.createCategory} />
+           <CategoryList categories={this.props.categories}
+                        categoryUpdate={this.props.categoryUpdate}
+                        categoryDestroy={this.props.categoryDestroy}
+           />
+        </div>
+    }
+};
+
+const mapStateToProps = state => ({
+    appName: state.appName,
+    categories: state.categories
+});
+
+const mapDispatchToProps = (dispatch, getState) => {
+    return {
+        categoryCreate: (data) => dispatch(categoryCreate(data)),
+        categoryUpdate: (data) => dispatch(categoryUpdate(data)),
+        categoryDestroy: (data) => dispatch(categoryDestroy(data)),
+    }
+};
+
+export default connectm(mapStateToProps, mapDispatchToProps)(dashboard);
